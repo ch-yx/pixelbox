6cb9dfb50ba95ebd9e293635cf4451c45bc6517d
ch-yx <cuiweixiaoxue63@126.com>
2023-05-25 00:45:34 +0800

    starting to use shader

--- src/main/java/com/example/Renderer.java
+++ src/main/java/com/example/Renderer.java
@@ -10,15 +10,19 @@ import com.mojang.blaze3d.vertex.VertexFormat;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider.Context;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.Resource;
+import net.minecraft.server.packs.resources.ResourceProvider;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.phys.Vec3;
+
+import java.io.IOException;
+import java.util.Optional;
 
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
@@ -30,9 +34,32 @@ public class Renderer<T extends Entity>
         extends EntityRenderer<T> {
     static Tesselator tessellator = Tesselator.getInstance();
     static BufferBuilder bufferBuilder = tessellator.getBuilder();
-    static Minecraft ins=Minecraft.getInstance();
+    static Minecraft ins = Minecraft.getInstance();
+
     public Renderer(Context context) {
         super(context);
+    }
+
+    static net.minecraft.client.renderer.ShaderInstance beam_shader;
+    static {
+        try {
+            beam_shader = new net.minecraft.client.renderer.ShaderInstance(
+                new ResourceProvider() {
+                    public Optional<Resource> getResource(ResourceLocation shadernamespacedname) {
+                        return Optional.ofNullable(new net.minecraft.server.packs.resources.Resource(null,
+                                () -> this.getClass().getClassLoader().getResource(
+                                        "assets/" + shadernamespacedname.getNamespace() + "/"
+                                                + shadernamespacedname.getPath())
+                                        .openStream()) {
+                            public String sourcePackId() {
+                                return "this.source.packId()";
+                            }
+                        });
+                    }
+                }, "beam",DefaultVertexFormat.POSITION_COLOR);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -53,107 +80,114 @@ public class Renderer<T extends Entity>
         RenderSystem.disableCull();
         RenderSystem.enableDepthTest();
 
-        //PoseStack matrixStack = RenderSystem.getModelViewStack();
-        //matrixStack.pushPose();
-        //matrixStack.mulPoseMatrix(poseStack.last().pose());
-        //RenderSystem.applyModelViewMatrix();
+        // PoseStack matrixStack = RenderSystem.getModelViewStack();
+        // matrixStack.pushPose();
+        // matrixStack.mulPoseMatrix(poseStack.last().pose());
+        // RenderSystem.applyModelViewMatrix();
 
-        //bufferBuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_COLOR);
+        // bufferBuilder.begin(VertexFormat.Mode.QUADS,
+        // DefaultVertexFormat.POSITION_COLOR);
 
         var color = ((PixelEntity) childentity).getpixelcolor();
 
-        drawBoxFaces(poseStack.last().pose(),bufferBuilder, -com.example.ExampleMod.pixsize / 2, 0,
+        drawBoxFaces(poseStack.last().pose(), bufferBuilder, -com.example.ExampleMod.pixsize / 2, 0,
                 -com.example.ExampleMod.pixsize / 2,
                 com.example.ExampleMod.pixsize / 2, com.example.ExampleMod.pixsize,
                 com.example.ExampleMod.pixsize / 2, color.x, color.y, color.z, 0.8f);
 
-        var con=((PixelEntity) childentity).getconer();
-        if (con!=null&&con.getId()>childentity.getId()) {
-            RenderSystem.setShader(GameRenderer::getPositionColorShader);
+        var con = ((PixelEntity) childentity).getconer();
+        if (con != null && con.getId() > childentity.getId()) {
+            RenderSystem.setShader(() -> beam_shader);// GameRenderer::getPositionColorShader);
             RenderSystem.enableDepthTest();
             RenderSystem.depthFunc(515);
             RenderSystem.enableBlend();
             RenderSystem.defaultBlendFunc();
             this.bufferBuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_COLOR);
-            
-            //var ssssss = Float.floatToIntBits(g)^childentity.tickCount;
-            var v =ins.gameRenderer.getMainCamera().getPosition().subtract(childentity.position());
+
+            // var ssssss = Float.floatToIntBits(g)^childentity.tickCount;
+            var v = ins.gameRenderer.getMainCamera().getPosition().subtract(childentity.position());
             if (((PixelEntity) childentity).isattacking()) {
-                drawline(poseStack.last().pose(),v,this.bufferBuilder, 0f, com.example.ExampleMod.pixsize / 2, 0f,
-                (float)(con.getX()-childentity.getX()), (float)(com.example.ExampleMod.pixsize / 2+con.getY()-childentity.getY()),(float) (con.getZ()-childentity.getZ()), 1.0f, 0.0f, 0.0f, 0.8f,ExampleMod.pixsize/2);
+                drawline(poseStack.last().pose(), v, this.bufferBuilder, 0f, com.example.ExampleMod.pixsize / 2, 0f,
+                        (float) (con.getX() - childentity.getX()),
+                        (float) (com.example.ExampleMod.pixsize / 2 + con.getY() - childentity.getY()),
+                        (float) (con.getZ() - childentity.getZ()), 1.0f, 0.0f, 0.0f, 0.8f, ExampleMod.pixsize / 2);
             } else {
-                drawline(poseStack.last().pose(),v,this.bufferBuilder, 0f, com.example.ExampleMod.pixsize / 2, 0f,
-                (float)(con.getX()-childentity.getX()), (float)(com.example.ExampleMod.pixsize / 2+con.getY()-childentity.getY()),(float) (con.getZ()-childentity.getZ()), 1.0f, 0.0f, 0.0f, 0.5f,ExampleMod.pixsize/4);
+                drawline(poseStack.last().pose(), v, this.bufferBuilder, 0f, com.example.ExampleMod.pixsize / 2, 0f,
+                        (float) (con.getX() - childentity.getX()),
+                        (float) (com.example.ExampleMod.pixsize / 2 + con.getY() - childentity.getY()),
+                        (float) (con.getZ() - childentity.getZ()), 1.0f, 0.0f, 0.0f, 0.5f, ExampleMod.pixsize / 4);
             }
             tessellator.end();
         }
-        //tessellator.end();
+        // tessellator.end();
 
-        //matrixStack.popPose();
-        //RenderSystem.applyModelViewMatrix();
+        // matrixStack.popPose();
+        // RenderSystem.applyModelViewMatrix();
     }
-
-    
 
     public static void drawBoxFaces(Matrix4f mat, VertexConsumer builder,
             float x1, float y1, float z1,
             float x2, float y2, float z2,
             float red1, float grn1, float blu1, float alpha) {
 
-        builder.vertex( mat, x1, y1, z1).color(red1, grn1, blu1, alpha).endVertex();
-        builder.vertex( mat, x2, y1, z1).color(red1, grn1, blu1, alpha).endVertex();
-        builder.vertex( mat, x2, y2, z1).color(red1, grn1, blu1, alpha).endVertex();
-        builder.vertex( mat, x1, y2, z1).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x1, y1, z1).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x2, y1, z1).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x2, y2, z1).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x1, y2, z1).color(red1, grn1, blu1, alpha).endVertex();
 
-        builder.vertex( mat, x1, y1, z2).color(red1, grn1, blu1, alpha).endVertex();
-        builder.vertex( mat, x1, y2, z2).color(red1, grn1, blu1, alpha).endVertex();
-        builder.vertex( mat, x2, y2, z2).color(red1, grn1, blu1, alpha).endVertex();
-        builder.vertex( mat, x2, y1, z2).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x1, y1, z2).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x1, y2, z2).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x2, y2, z2).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x2, y1, z2).color(red1, grn1, blu1, alpha).endVertex();
 
-        builder.vertex( mat, x1, y1, z1).color(red1, grn1, blu1, alpha).endVertex();
-        builder.vertex( mat, x1, y2, z1).color(red1, grn1, blu1, alpha).endVertex();
-        builder.vertex( mat, x1, y2, z2).color(red1, grn1, blu1, alpha).endVertex();
-        builder.vertex( mat, x1, y1, z2).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x1, y1, z1).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x1, y2, z1).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x1, y2, z2).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x1, y1, z2).color(red1, grn1, blu1, alpha).endVertex();
 
-        builder.vertex( mat, x2, y1, z1).color(red1, grn1, blu1, alpha).endVertex();
-        builder.vertex( mat, x2, y1, z2).color(red1, grn1, blu1, alpha).endVertex();
-        builder.vertex( mat, x2, y2, z2).color(red1, grn1, blu1, alpha).endVertex();
-        builder.vertex( mat, x2, y2, z1).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x2, y1, z1).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x2, y1, z2).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x2, y2, z2).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x2, y2, z1).color(red1, grn1, blu1, alpha).endVertex();
 
-        builder.vertex( mat, x1, y1, z1).color(red1, grn1, blu1, alpha).endVertex();
-        builder.vertex( mat, x2, y1, z1).color(red1, grn1, blu1, alpha).endVertex();
-        builder.vertex( mat, x2, y1, z2).color(red1, grn1, blu1, alpha).endVertex();
-        builder.vertex( mat, x1, y1, z2).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x1, y1, z1).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x2, y1, z1).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x2, y1, z2).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x1, y1, z2).color(red1, grn1, blu1, alpha).endVertex();
 
-        builder.vertex( mat, x1, y2, z1).color(red1, grn1, blu1, alpha).endVertex();
-        builder.vertex( mat, x2, y2, z1).color(red1, grn1, blu1, alpha).endVertex();
-        builder.vertex( mat, x2, y2, z2).color(red1, grn1, blu1, alpha).endVertex();
-        builder.vertex( mat, x1, y2, z2).color(red1, grn1, blu1, alpha).endVertex();
-                ///
-        
-       
+        builder.vertex(mat, x1, y2, z1).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x2, y2, z1).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x2, y2, z2).color(red1, grn1, blu1, alpha).endVertex();
+        builder.vertex(mat, x1, y2, z2).color(red1, grn1, blu1, alpha).endVertex();
+        ///
+
     }
-  
 
-    public static void drawline(Matrix4f mat, Vec3 v,VertexConsumer builder,
+    public static void drawline(Matrix4f mat, Vec3 v, VertexConsumer builder,
             float x1, float y1, float z1,
             float x2, float y2, float z2,
             float red1, float grn1, float blu1, float alpha, float linew) {
-        
-        //RenderSystem.getInverseViewRotationMatrix().getColumn(2, front);
-        
-        Vector3f front=new Vector3f((float)v.x,(float)v.y,(float)v.z);
-        front.cross(x1-x2, y1-y2, z1-z2).normalize(linew);
-        var fx=front.x; var fy=front.y; var fz=front.z;
-        float step=-1.0f;
-        float al=Math.min(0.2f/(step*step),1.0f);
-        for(;step<=0.99;){
-            builder.vertex( mat, x1+fx*step, y1+fy*step, z1+fz*step).color(red1, grn1, blu1, alpha*al).endVertex();
-            builder.vertex( mat, x2+fx*step, y2+fy*step, z2+fz*step).color(red1, grn1, blu1, alpha*al).endVertex();
-            step+=0.02f;
-            al=Math.min(0.2f/(step*step),1.0f);
-            builder.vertex( mat, x2+fx*step, y2+fy*step, z2+fz*step).color(red1, grn1, blu1, alpha*al).endVertex();
-            builder.vertex( mat, x1+fx*step, y1+fy*step, z1+fz*step).color(red1, grn1, blu1, alpha*al).endVertex();
+
+        // RenderSystem.getInverseViewRotationMatrix().getColumn(2, front);
+
+        Vector3f front = new Vector3f((float) v.x, (float) v.y, (float) v.z);
+        front.cross(x1 - x2, y1 - y2, z1 - z2).normalize(linew);
+        var fx = front.x;
+        var fy = front.y;
+        var fz = front.z;
+        float step = -1.0f;
+        float al = Math.min(0.2f / (step * step), 1.0f);
+        for (; step <= 0.99;) {
+            builder.vertex(mat, x1 + fx * step, y1 + fy * step, z1 + fz * step).color(red1, grn1, blu1, alpha * al)
+                    .endVertex();
+            builder.vertex(mat, x2 + fx * step, y2 + fy * step, z2 + fz * step).color(red1, grn1, blu1, alpha * al)
+                    .endVertex();
+            step += 0.02f;
+            al = Math.min(0.2f / (step * step), 1.0f);
+            builder.vertex(mat, x2 + fx * step, y2 + fy * step, z2 + fz * step).color(red1, grn1, blu1, alpha * al)
+                    .endVertex();
+            builder.vertex(mat, x1 + fx * step, y1 + fy * step, z1 + fz * step).color(red1, grn1, blu1, alpha * al)
+                    .endVertex();
         }
-            }
+    }
 }
